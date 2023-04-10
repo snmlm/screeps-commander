e6738702b7d2165227db6eb831241bc96cc07221
snmlm <913813112@qq.com>
2023-04-11 00:31:24 +0800

    update

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Qs from 'qs'
 
 const servers: ServerList = {
-    screeps: 'https://screeps-cros.herokuapp.com/https://screeps.com'
+    screeps: 'https://screeps.com'
 }
 
 axios.interceptors.request.use(axiosConfig => {
